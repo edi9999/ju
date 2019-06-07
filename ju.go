18938c9fec20d4a89d8e5e5d9e1ad2c95d9c2be7
Edgar HIPP <hipp.edg@gmail.com>
2019-06-07 16:15:13 +0200

    Update ju

--- ju.go
+++ ju.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"strconv"
 )
 
 type State struct {
 	Full_text string `json:"full_text"`
 	Color     string `json:"color"`
 	Name      string `json:"name"`
+	MinWidth  int    `json:"min_width"`
 }
 
 func main() {
@@ -19,13 +21,15 @@ func main() {
 	states := []State{}
 
 	for i < len(args) {
+		mw, _ := strconv.Atoi(args[i+3])
 		s := State{
 			Full_text: args[i],
 			Name:      args[i+1],
 			Color:     args[i+2],
+			MinWidth:  mw,
 		}
 		states = append(states, s)
-		i = i + 3
+		i = i + 4
 	}
 	a, _ := json.Marshal(states)
 	fmt.Printf("%s\n", a)
